fa5b59ca6d9e400677a8b5cf017ec38ab3d58ab0
Yuu Shimizu <p@yuushimizu.com>
2019-06-16 21:16:52 +0900

    add axis ops

--- src/axis.rs
+++ src/axis.rs
@@ -28,9 +28,8 @@ pub trait AxisKeyed: Coord {
     /// # use coord::Vector;
     /// # use coord::Axis;
     /// # use coord::AxisKeyed;
-    /// let v = Vector::new(30, 50);
-    /// assert_eq!(30, v.get(Axis::X));
-    /// assert_eq!(50, v.get(Axis::Y));
+    /// assert_eq!(30, Vector::new(30, 50).get(Axis::X));
+    /// assert_eq!(50, Vector::new(30, 50).get(Axis::Y));
     /// ```
     fn get(&self, axis: Axis) -> Self::Item {
         match axis {
@@ -44,7 +43,6 @@ pub trait AxisKeyed: Coord {
     /// # use coord::Point;
     /// # use coord::Axis;
     /// # use coord::AxisKeyed;
-    /// let p = Point::new(5, 8);
     /// assert_eq!(Point::new(10, 8), Point::new(5, 8).set(Axis::X, 10));
     /// assert_eq!(Point::new(5, 10), Point::new(5, 8).set(Axis::Y, 10));
     /// ```
@@ -60,15 +58,56 @@ pub trait AxisKeyed: Coord {
     /// # use coord::Point;
     /// # use coord::Axis;
     /// # use coord::AxisKeyed;
-    /// let p = Point::new(10, 20);
     /// assert_eq!(Point::new(12, 20), Point::new(10, 20).add(Axis::X, 2));
     /// assert_eq!(Point::new(10, 22), Point::new(10, 20).add(Axis::Y, 2));
     /// ```
-    fn add<T>(&self, axis: Axis, amount: T) -> Self
+    fn add<T>(&self, axis: Axis, value: T) -> Self
     where
         Self::Item: ops::Add<T, Output = Self::Item>,
     {
-        self.set(axis, self.get(axis) + amount)
+        self.set(axis, self.get(axis) + value)
+    }
+
+    /// # Examples
+    /// ```
+    /// # use coord::Point;
+    /// # use coord::Axis;
+    /// # use coord::AxisKeyed;
+    /// assert_eq!(Point::new(8, 20), Point::new(10, 20).sub(Axis::X, 2));
+    /// assert_eq!(Point::new(10, 17), Point::new(10, 20).sub(Axis::Y, 3));
+    /// ```
+    fn sub<T>(&self, axis: Axis, value: T) -> Self
+    where
+        Self::Item: ops::Sub<T, Output = Self::Item>,
+    {
+        self.set(axis, self.get(axis) - value)
+    }
+
+    /// # Examples
+    /// ```
+    /// # use coord::Point;
+    /// # use coord::Axis;
+    /// # use coord::AxisKeyed;
+    /// assert_eq!(Point::new(12, 5), Point::new(4, 5).mul(Axis::X, 3));
+    /// assert_eq!(Point::new(4, 20), Point::new(4, 5).mul(Axis::Y, 4));
+    /// ```
+    fn mul<T>(&self, axis: Axis, value: T) -> Self
+    where
+        Self::Item: ops::Mul<T, Output = Self::Item>,
+    {
+        self.set(axis, self.get(axis) * value)
+    }
+
+    /// # Examples
+    /// ```
+    /// # use coord::Point;
+    /// # use coord::Axis;
+    /// # use coord::AxisKeyed;
+    /// assert_eq!(Point::new(4, 20), Point::new(12, 20).div(Axis::X, 3));
+    /// assert_eq!(Point::new(12, 5), Point::new(12, 20).div(Axis::Y, 4));
+    /// ```
+    fn div<T>(&self, axis: Axis, value: T) -> Self where Self::Item: ops::Div<T, Output = Self::Item> {
+        self.set(axis, self.get(axis) / value)
     }
 }
 
