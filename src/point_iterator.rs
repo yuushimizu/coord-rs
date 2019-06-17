a84f1149287786a21d31eb1de81eb8b4d8deb8ba
Yuu Shimizu <p@yuushimizu.com>
2019-06-18 02:45:26 +0900

    add derive to PointIterator

--- src/point_iterator.rs
+++ src/point_iterator.rs
@@ -5,19 +5,23 @@ use crate::point::Point;
 use crate::vector::Vector;
 use std::ops;
 
-pub trait PointStep<S: Primitive = Self>: Primitive + PartialOrd + ops::Add<S, Output = Self> + num::One {}
+pub trait PointStep<S: Primitive = Self>:
+    Primitive + PartialOrd + ops::Add<S, Output = Self> + num::One
+{
+}
 
-impl<S: Primitive, T: Primitive + PartialOrd + ops::Add<S, Output = Self> + num::One> PointStep<S> for T {}
+impl<S: Primitive, T: Primitive + PartialOrd + ops::Add<S, Output = Self> + num::One> PointStep<S>
+    for T
+{
+}
 
 fn next<S: Primitive, T: PointStep<S>>(
     start: Point<T>,
     end: Point<T>,
     step: Vector<S>,
     current: &mut Point<T>,
-    compare: fn(T, T) -> bool
-) -> Option<Point<T>>
-
-{
+    compare: fn(T, T) -> bool,
+) -> Option<Point<T>> {
     while compare(current.y(), end.y()) {
         if compare(current.x(), end.x()) {
             let result = Some(*current);
@@ -29,6 +33,7 @@ fn next<S: Primitive, T: PointStep<S>>(
     None
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct PointIterator<T: Primitive, S: Primitive> {
     start: Point<T>,
     end: Point<T>,
@@ -47,15 +52,21 @@ impl<T: Primitive, S: Primitive> PointIterator<T, S> {
     }
 }
 
-impl<S: Primitive, T: PointStep<S>> Iterator for PointIterator<T, S>
-{
+impl<S: Primitive, T: PointStep<S>> Iterator for PointIterator<T, S> {
     type Item = Point<T>;
 
     fn next(&mut self) -> Option<Point<T>> {
-        next(self.start, self.end, self.step, &mut self.current, |n, m| n < m)
+        next(
+            self.start,
+            self.end,
+            self.step,
+            &mut self.current,
+            |n, m| n < m,
+        )
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct PointIteratorInclusive<T: Primitive, S: Primitive> {
     start: Point<T>,
     end: Point<T>,
@@ -74,11 +85,16 @@ impl<T: Primitive, S: Primitive> PointIteratorInclusive<T, S> {
     }
 }
 
-impl<S: Primitive, T: PointStep<S>> Iterator for PointIteratorInclusive<T, S>
-{
+impl<S: Primitive, T: PointStep<S>> Iterator for PointIteratorInclusive<T, S> {
     type Item = Point<T>;
 
     fn next(&mut self) -> Option<Point<T>> {
-        next(self.start, self.end, self.step, &mut self.current, |n, m| n <= m)
+        next(
+            self.start,
+            self.end,
+            self.step,
+            &mut self.current,
+            |n, m| n <= m,
+        )
     }
 }
